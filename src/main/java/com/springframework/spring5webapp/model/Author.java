5ad1550b7c8a685267fa2e8c258ee10d048edcc0
Nkululeko Maseko <nkululekob.maseko@gmail.com>
2023-08-27 00:57:19 +0200

    feature: Adding JPA Ids

--- src/main/java/com/springframework/spring5webapp/model/Author.java
+++ src/main/java/com/springframework/spring5webapp/model/Author.java
@@ -1,8 +1,19 @@
 package com.springframework.spring5webapp.model;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String firstName;
     private String lastName;
     private Set<Book> books;
@@ -14,6 +25,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
