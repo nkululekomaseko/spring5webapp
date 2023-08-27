260655a6b003c63657bca5ff8729386fe7c383fd
Nkululeko Maseko <nkululekob.maseko@gmail.com>
2023-08-27 02:17:39 +0200

    feat: Creating Publisher model and repository

---
+++ src/main/java/com/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,108 @@
+package com.springframework.spring5webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String address_line_1;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String address_line_1, String city, String state, String zip) {
+        this.name = name;
+        this.address_line_1 = address_line_1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress_line_1() {
+        return address_line_1;
+    }
+
+    public void setAddress_line_1(String address_line_1) {
+        this.address_line_1 = address_line_1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "name='" + name + '\'' +
+                ", address_line_1='" + address_line_1 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        if (!Objects.equals(name, publisher.name)) return false;
+        if (!Objects.equals(address_line_1, publisher.address_line_1))
+            return false;
+        if (!Objects.equals(city, publisher.city)) return false;
+        if (!Objects.equals(state, publisher.state)) return false;
+        return Objects.equals(zip, publisher.zip);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (address_line_1 != null ? address_line_1.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        result = 31 * result + (zip != null ? zip.hashCode() : 0);
+        return result;
+    }
+}
